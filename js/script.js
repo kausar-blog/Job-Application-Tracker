345a134de832fa6d27a07a3167ccad7e3e3c1175
kausar-blog <kausarprogrammer@gmail.com>
2026-02-22 14:39:29 +0600

    js count etc

--- js/script.js
+++ js/script.js
@@ -6,10 +6,12 @@ let totalJobsEl = document.getElementById("appTotal");
 let interviewCountEl = document.getElementById("appInterview");
 let rejectedCountEl = document.getElementById("appRejected");
 let jobsDisplayCountEl = document.getElementById("jobsCount");
+let visibleJobsCountEl = document.getElementById("visibleJobsCount");
 
 const btnFilterAll = document.getElementById("filterAll");
 const btnFilterInterview = document.getElementById("filterInterview");
 const btnFilterRejected = document.getElementById("filterRejected");
+const jobInfoCount = document.getElementById("job-info-count");
 
 const jobCardsContainer = document.getElementById("jobCards");
 const sectionFilteredJobs = document.getElementById("filteredIntRej");
@@ -19,6 +21,7 @@ const mainEl = document.querySelector("main");
 function calculateCount() {
   totalJobsEl.innerText = jobCardsContainer.children.length;
   jobsDisplayCountEl.innerText = jobCardsContainer.children.length;
+
   interviewCountEl.innerText = jobsInterviewed.length;
   rejectedCountEl.innerText = jobsRejected.length;
 }
@@ -28,10 +31,12 @@ calculateCount();
 // 2 button toggle & display
 
 function toggleStyle(id) {
+  // removing color for all
   btnFilterAll.classList.remove("btn-primary");
   btnFilterInterview.classList.remove("btn-primary");
   btnFilterRejected.classList.remove("btn-primary");
 
+  // adding color for all
   btnFilterAll.classList.add("btn-outline");
   btnFilterInterview.classList.add("btn-outline");
   btnFilterRejected.classList.add("btn-outline");
@@ -47,13 +52,18 @@ function toggleStyle(id) {
   if (id == "filterInterview") {
     jobCardsContainer.classList.add("hidden");
     sectionFilteredJobs.classList.remove("hidden");
+    jobInfoCount.classList.remove("hidden");
+
     renderInterview();
   } else if (id == "filterAll") {
     jobCardsContainer.classList.remove("hidden");
     sectionFilteredJobs.classList.add("hidden");
+    jobInfoCount.classList.add("hidden");
   } else if (id == "filterRejected") {
     jobCardsContainer.classList.add("hidden");
     sectionFilteredJobs.classList.remove("hidden");
+    jobInfoCount.classList.remove("hidden");
+
     renderRejected();
   }
 }
@@ -66,7 +76,7 @@ mainEl.addEventListener("click", (event) => {
     const jobCompany = parentNode.querySelector(".jobCompany").innerText;
     const jobPosition = parentNode.querySelector(".jobPosition").innerText;
     const jobDetails = parentNode.querySelector(".jobDetails").innerText;
-    let jobStatus = parentNode.querySelector(".jobStatus").innerText;
+    const jobStatus = parentNode.querySelector(".jobStatus").innerText;
     const jobNotes = parentNode.querySelector(".jobNotes").innerText;
 
     const jobInfo = {
@@ -77,7 +87,7 @@ mainEl.addEventListener("click", (event) => {
       jobNotes,
     };
 
-    jobStatus = "Interview";
+    parentNode.querySelector(".jobStatus").innerText = "Interview";
     // console.log(jobInfo);
 
     const jobExist = jobsInterviewed.find(
@@ -103,8 +113,10 @@ mainEl.addEventListener("click", (event) => {
     const jobCompany = parentNode.querySelector(".jobCompany").innerText;
     const jobPosition = parentNode.querySelector(".jobPosition").innerText;
     const jobDetails = parentNode.querySelector(".jobDetails").innerText;
-    let jobStatus = parentNode.querySelector(".jobStatus").innerText;
+    const jobStatus = parentNode.querySelector(".jobStatus").innerText;
     const jobNotes = parentNode.querySelector(".jobNotes").innerText;
+
+    parentNode.querySelector(".jobStatus").innerText = "Rejected";
 
     const jobInfo = {
       jobCompany,
@@ -114,7 +126,6 @@ mainEl.addEventListener("click", (event) => {
       jobNotes,
     };
 
-    jobStatus = "Interview";
     // console.log(jobInfo);
 
     const jobExist = jobsRejected.find(
@@ -140,6 +151,7 @@ mainEl.addEventListener("click", (event) => {
 // 4 new html file created
 function renderInterview() {
   sectionFilteredJobs.innerHTML = "";
+  visibleJobsCountEl.innerText = jobsInterviewed.length;
 
   for (let interview of jobsInterviewed) {
     // console.log(interview);
@@ -148,7 +160,40 @@ function renderInterview() {
     div.classList =
       "flex flex-col md:flex-row justify-between border rounded-lg p-4 bg-base-100";
 
-    div.innerHTML = `<p>Hello Kausar ${interview.jobCompany}</p>`;
+    div.innerHTML = `
+     <div class="space-y-4 md:space-y-6 flex-1">
+            <!-- title and position -->
+            <div>
+              <p class="jobCompany text-2xl md:text-3xl font-bold mb-3"
+                >${interview.jobCompany}</p
+              >
+              <p class="jobPosition text-gray-600">${interview.jobPosition}</p>
+            </div>
+            <!-- location / type / salary -->
+            <div class="jobDetails text-gray-600 text-sm md:text-base">
+              ${interview.jobDetails}
+            </div>
+            <!-- status -->
+            <p class="jobStatus text-gray-500 font-medium">${interview.jobStatus}</p>
+
+            <!-- notes  -->
+            <p class="jobNotes text-gray-700"
+              >${interview.jobNotes}</p
+            >
+
+            <!-- actions buttons -->
+            <div class="flex flex-wrap gap-3">
+              <button class="interview-btn btn btn-success btn-sm" type="button"
+                >Interview</button
+              >
+              <button class="rejected-btn btn btn-error btn-sm" type="button"
+                >Rejected</button
+              >
+            </div>
+          </div>
+    
+    
+    `;
 
     sectionFilteredJobs.appendChild(div);
   }
@@ -156,6 +201,7 @@ function renderInterview() {
 
 function renderRejected() {
   sectionFilteredJobs.innerHTML = "";
+  visibleJobsCountEl.innerText = jobsRejected.length;
 
   for (let rejected of jobsRejected) {
     // console.log(interview);
@@ -164,7 +210,40 @@ function renderRejected() {
     div.classList =
       "flex flex-col md:flex-row justify-between border rounded-lg p-4 bg-base-100";
 
-    div.innerHTML = `<p>Hello blogger ${rejected.jobCompany}</p>`;
+    div.innerHTML = `
+     <div class="space-y-4 md:space-y-6 flex-1">
+            <!-- title and position -->
+            <div>
+              <p class="jobCompany text-2xl md:text-3xl font-bold mb-3"
+                >${rejected.jobCompany}</p
+              >
+              <p class="jobPosition text-gray-600">${rejected.jobPosition}</p>
+            </div>
+            <!-- location / type / salary -->
+            <div class="jobDetails text-gray-600 text-sm md:text-base">
+              ${rejected.jobDetails}
+            </div>
+            <!-- status -->
+            <p class="jobStatus text-gray-500 font-medium">${rejected.jobStatus}</p>
+
+            <!-- notes  -->
+            <p class="jobNotes text-gray-700"
+              >${rejected.jobNotes}</p
+            >
+
+            <!-- actions buttons -->
+            <div class="flex flex-wrap gap-3">
+              <button class="interview-btn btn btn-success btn-sm" type="button"
+                >Interview</button
+              >
+              <button class="rejected-btn btn btn-error btn-sm" type="button"
+                >Rejected</button
+              >
+            </div>
+          </div>
+    
+    
+    `;
 
     sectionFilteredJobs.appendChild(div);
   }
